4ad697693bddd6d48cfa94adf7a4dc6eb346ed4d
Benjamin Nanes <bnanes@emory.edu>
2015-07-23 21:33:31 -0400

    Add Border Stats (multi-chan) command
    Add an alternate version of the Border Statistics command which operates
    on images with more than 3 channels.

---
+++ src/main/java/edu/emory/cellbio/ijbat/pi/BorderStatsNChan.java
@@ -0,0 +1,195 @@
+package edu.emory.cellbio.ijbat.pi;
+
+import edu.emory.cellbio.ijbat.ex.SlideSetException;
+import edu.emory.cellbio.ijbat.ui.SlideSetLog;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import net.imagej.Dataset;
+import net.imagej.axis.Axes;
+import net.imagej.overlay.AbstractOverlay;
+import net.imglib2.RandomAccess;
+import net.imglib2.iterator.IntervalIterator;
+import net.imglib2.roi.RegionOfInterest;
+import net.imglib2.type.numeric.RealType;
+import org.scijava.ItemIO;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+/**
+ * Calculate average values for pixels
+ * along region of interest borders
+ * 
+ * @author Benjamin Nanes
+ */
+@HelpPath(path = "plugins/borders.html#nchan")
+@Plugin(type=SlideSetPlugin.class,
+    label="Border Stats (multi-chan)", visible = false,
+    menuPath = "Plugins > Slide Set > Commands > Border Stats (multi-chan)")
+public class BorderStatsNChan extends SlideSetPlugin implements MultipleResults {
+    
+    @Parameter(label="Log", type=ItemIO.INPUT)
+    private SlideSetLog log;
+    
+    @Parameter(label = "Image", type = ItemIO.INPUT)
+    private Dataset ds;
+
+    @Parameter(label = "Region of Interest", type = ItemIO.INPUT)
+    private AbstractOverlay[] roi;
+    
+    @Parameter(label = "Border Width", type = ItemIO.INPUT)
+    private double w;
+    
+    @Parameter(label = "Thresholds", type = ItemIO.INPUT)
+    private String ts;
+
+    @Parameter(label = "Invert", type = ItemIO.INPUT)
+    private boolean inv;
+    
+    @Parameter(label = "Channel", type = ItemIO.OUTPUT)
+    private int[] chans;
+    
+    @Parameter(label = "Mean", type = ItemIO.OUTPUT)
+    private double[] means;
+    
+    @Parameter(label = "Length", type = ItemIO.OUTPUT)
+    private double[] length;
+
+    @Parameter(label = "Border Stripe Pixels", type = ItemIO.OUTPUT)
+    private double[] size;
+
+    public void run() {
+
+        if (roi == null || ds == null || roi.length == 0) {
+            chans = new int[0];
+            means = new double[0];
+            length = new double[0];
+            size = new double[0];
+            return;
+        }
+        final long[] dims = new long[ds.numDimensions()];
+        final boolean singlet = dims.length == 2;
+        ds.dimensions(dims);
+        final int cAxis = ds.dimensionIndex(Axes.CHANNEL);
+        final int xAxis = ds.dimensionIndex(Axes.X);
+        final int yAxis = ds.dimensionIndex(Axes.Y);
+        if (!singlet) {
+            if (xAxis < 0 || yAxis < 0)
+                throw new IllegalArgumentException("Could not get X and Y axes for " + ds.getName());
+            if (cAxis < 0) {
+                throw new IllegalArgumentException("Could not get channel axis index for " + ds.getName());
+            }
+            if (dims[cAxis] == 0) {
+                throw new IllegalArgumentException(ds.getName() + " has zero channels");
+            }
+        }
+        final int nChan = singlet ? 1 : (int)dims[cAxis];
+        IntervalIterator ii;
+        final double[] posD = new double[dims.length];
+        final double[] pos2D = new double[2];
+        final long[] min = new long[dims.length];
+        final long[] max = new long[dims.length];
+        RandomAccess<? extends RealType<?>> ra = ds.randomAccess();
+        ArrayList<Double> vArray = new ArrayList<Double>();
+        ArrayList<Double> sArray = new ArrayList<Double>();
+        ArrayList<Double> lArray = new ArrayList<Double>();
+        ArrayList<Integer> cArray = new ArrayList<Integer>();
+        //Parse threshold string
+        final double[] nts = new double[nChan];
+        Arrays.fill(nts, 0);
+        String[] tss = ts.split("\\s");
+        if(tss.length > 0) {
+            int j=0;
+            for(int i=0; i < nts.length; i++) {
+                if(j >= tss.length)
+                    j = 0;
+                try {
+                    nts[i] = new Double(tss[j]);
+                } catch(NumberFormatException e) { }
+                j++;
+            }
+        }
+        RegionOfInterest bin;
+        BigInteger[] v = new BigInteger[nChan];
+        double s;
+        //
+        for (int i = 0; i < roi.length; i++) {
+            bin = roi[i].getRegionOfInterest();
+            for (int c = 0; c < bin.numDimensions(); c++) {
+                min[c] = Math.round(Math.floor(bin.realMin(c) - w));
+                if(min[c] < 0 || min[c] > dims[c])
+                    min[c] = 0;
+                max[c] = Math.round(Math.ceil(bin.realMax(c) + w));
+                if(max[c] < 0 || max[c] > dims[c])
+                    max[c] = dims[c];
+            }
+            if (!singlet) {
+                min[cAxis] = 0;
+                max[cAxis] = 0;
+            }
+            s = 0;
+            Arrays.fill(v, BigInteger.valueOf(0));
+            ii = new IntervalIterator(min, max);
+            try {
+                while (ii.hasNext()) {
+                    ii.fwd();
+                    ii.localize(posD);
+                    pos2D[0] = posD[xAxis];
+                    pos2D[1] = posD[yAxis];
+                    if(!RoiUtils.isNearBorder(pos2D, roi[i], w) || !inBounds(posD, dims))
+                        continue;
+                    ra.setPosition(ii);
+                    ++s;
+                    for(int j=0; j<nChan; j++) {
+                        if(!singlet)
+                            ra.setPosition(j, cAxis);
+                        if (inv) {
+                            v[j] = v[j].subtract(BigInteger.valueOf(Math.round(Math.min(ra.get().getRealDouble() - nts[j], 0))));
+                        } else {
+                            v[j] = v[j].add(BigInteger.valueOf(Math.round(Math.max(ra.get().getRealDouble() - nts[j], 0))));
+                        }
+                    }
+                }
+            } catch(SlideSetException e) {
+                log.println("Warning: ROI number " + String.valueOf(i) + " is not compatible");
+                log.println("   with this command and will be skipped.");
+                continue;
+            }
+            double l;
+            try {
+                l = ROILengths.getRoiLenth(roi[i]);
+            } catch(SlideSetException e) {
+                log.println("Warning: " + e.getMessage());
+                l = 0;
+            }
+            for(int j=0; j<nChan; j++) {
+                sArray.add(s);
+                vArray.add(v[j].doubleValue() / s);
+                lArray.add(l);
+                cArray.add(j);
+            }
+        }
+        int n = vArray.size();
+        means = new double[n];
+        size = new double[n];
+        length = new double[n];
+        chans = new int[n];
+        for(int i = 0; i < n; i++) {
+            means[i] = vArray.get(i);
+            size[i] = sArray.get(i);
+            length[i] = lArray.get(i);
+            chans[i] = cArray.get(i);
+        }
+    }
+    
+    /**
+     * Check if a point is within the image dimensions
+     */
+    private boolean inBounds(double[] pos, long[] dims) {
+        for(int i = 0; i < pos.length; i++)
+            if(pos[i] >= dims[i] || pos[i] < 0)
+                return false;
+        return true;
+    }
+    
+}
--- src/site/markdown/plugins/borders.md
+++ src/site/markdown/plugins/borders.md
@@ -3,14 +3,17 @@ Border Statistics
 
 Calculate signal intensity within sets
 of pixels defined by the borders of
-regions of interest (ROIs) in up to
-3-channel RGB images. Returns the mean
+regions of interest (ROIs). Returns the mean
 value on each channel, the length of
 the border, and the number of pixels
 included in the border stripe.
+This command has two variants, [standard](#std) (`Border Statistics`)
+and [multi-channel](#nchan) (`Border Stats (multi-chan)`),
+with different structures for input parameters and results.
 
-Input Parameters
-----------------
+<h2 id="std">Standard</h2>
+
+### Input Parameters
 
 #### Image
 
@@ -48,8 +51,7 @@ threshold (`true`).
 
 Type: Logical (`boolean`)
 
-Results
--------
+### Results
 
 One result row is produced for each ROI.
 
@@ -58,6 +60,96 @@ One result row is produced for each ROI.
 Mean of threshold-subtracted pixel values within the ROI.
 For green and blue totals, returns 0 if the image
 does not include a second or third channel.
+
+If `Invert` is `false` (default):
+
+<pre>&sum;[max(X<sub>i,c</sub> - T<sub>c</sub>, 0)] / area</pre>
+
+If `Invert` is `true`:
+
+<pre>&ndash;&sum;[min(X<sub>i,c</sub> - T<sub>c</sub>, 0)] / area</pre>
+ 
+<code>X<sub>i,c</sub></code>, value at pixel `i` on channel `c`    
+<code>T<sub>c</sub></code>, threshold for channel `c`    
+<code>area</code>, number of pixels included in the border region
+
+Type: Numeric (`double`)
+
+#### Length
+
+Length, in pixels, of the border.
+
+Type: Numeric (`double`)
+
+#### Border Stripe Pixels
+
+Number of pixels included within the border region.
+
+Type: Integer (`int`)
+
+
+
+<h2 id="nchan">Multi-channel</h2>
+
+### Input Parameters
+
+#### Image
+
+Expects arbitrary bit-depth images with any number of channels.
+
+Type: Image File (`imagej.data.Dataset`)
+
+#### Region of Interest
+
+Set of ROIs defining the region borders.
+
+Type: ROI Set (file) (`imagej.data.overlay.AbstractOverlay[]`)
+
+#### Border Width
+
+The distance, in pixels, from the ROI borders that will
+be included in the analysis.
+
+Type: Numeric (`double`)
+
+#### Thresholds
+
+A space-separated ordered list of thresholds for each channel. The appropriate
+threshold is subtracted from each pixel value. If the pixel
+value is less than the threshold, the pixel value is counted as 0.
+
+For example, `10 25 102` would specify a threshold of 10 for the first
+channel, a threshold of 25 for the second channel, and a threshold of 102
+for the third channel. If an image has fewer channels than
+the number of thresholds listed, the extra threshold values will not
+be used. If an image has more channels than the number of thresholds
+listed, they will be recycled in order. If no thresholds are listed,
+a threshold of `0` is implied.
+
+Type: Text (`String`)
+
+#### Invert
+
+Flag specifying whether pixel values should be measured
+above the threshold (`false`, default) or below the
+threshold (`true`).
+
+Type: Logical (`boolean`)
+
+### Results
+
+One result row is produced for each image channel within each ROI.
+
+#### Channel
+
+0-based index of the image channel measured.
+
+Type: Integer (`int`)
+
+#### Mean
+
+Mean of threshold-subtracted pixel values within `Border Width`
+pixels of the ROI border.
 
 If `Invert` is `false` (default):
 
